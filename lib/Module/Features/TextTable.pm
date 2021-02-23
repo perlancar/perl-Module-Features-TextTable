022e8b8832c13a890fae35fc0cb8cf2772ea3989
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2021-02-23 22:01:22 +0700

    Initial commit

---
+++ lib/Module/Features/TextTable.pm
@@ -0,0 +1,23 @@
+package Module::Features::TextTable;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+use 5.010001;
+use strict;
+use warnings;
+
+our %FEATURES_DEF = (
+    summary => 'Features of modules that generate text tables',
+    features => {
+    },
+);
+
+1;
+# ABSTRACT: Features of modules that generate text tables
+
+=head1 SEE ALSO
+
+L<Module::Features>
